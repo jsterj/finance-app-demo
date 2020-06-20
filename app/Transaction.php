e3327a0d9fb1334532f9b79e674254c739abf309
Justin Donnell <jsterj@users.noreply.github.com>
2020-06-20 15:27:42 -0400

    Added user/transaction relationships

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+  /**
+  * The user that this transaction belongs to
+  */
+  public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
 }
